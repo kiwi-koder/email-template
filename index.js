d3fcd0e7932357212c496d090b57efa43dfdc5b5
Joshua Rippon <joshuarippon1@gmail.com>
2018-06-02 14:06:40 +0100

    Template email sends

--- index.js
+++ index.js
@@ -1,3 +1,54 @@
 require("env2")(".env");
+const Email = require("email-templates");
+const nodemailer = require("nodemailer");
+const path = require("path");
 
-console.log(process.env.PASS);
+let transporter = nodemailer.createTransport({
+  secure: true,
+  service: process.env.PROVIDER,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASS
+  }
+});
+
+let users = [
+  {
+    name: "Josh",
+    email: "joshuarippon1@gmail.com"
+  }
+];
+
+const loadTemplate = (templateName, contexts) => {
+  let email = new Email({
+    message: {
+      from: process.env.EMAIL
+    },
+    views: {
+      root: path.join(__dirname, "templates"),
+      options: {
+        extension: "hbs"
+      },
+      transport: transporter,
+      send: true
+    }
+  });
+
+  return Promise.all(
+    contexts.map(context => {
+      return email.renderAll(
+        templateName,
+
+        context
+      );
+    })
+  ).catch(err => console.log(err));
+};
+
+loadTemplate("welcome", users).then(result => {
+  return transporter.sendMail({
+    from: process.env.EMAIL,
+    to: "joshuarippon1@gmail.com",
+    ...result[0]
+  });
+});
